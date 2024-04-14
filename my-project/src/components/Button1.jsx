bc99fae4da8b8e08822c9386a25e6186e9519704
Sagar Sharma <sagarsharma2809@gmail.com>
2024-04-15 02:13:43 +0530

    completed my button task, active, standard, hover

--- my-project/src/components/Button1.jsx
+++ my-project/src/components/Button1.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+
+
+
 
 export const Button1 = () => {
+  const [buttonText, setButtonText] = useState("BUTTON");
+  const [backgroundColor, setBackgroundColor] = useState("bg-sky-300 hover:bg-sky-500 active:bg-green-400 ");
+
+  const changeButton = () => {
+    setButtonText("DONE");
+    setBackgroundColor("bg-green-400");
+  }
+
   return (
     <>
-      <button className='bg-teal-600  py-2 w-1/6'>Button</button>
+      <button className={`${backgroundColor} py-4 w-1/6 rounded-full text-xs text-white tracking-[0.4em] drop-shadow-lg`} onClick={changeButton}>{buttonText}</button >
     </>
   );
 }
